34749aa8af8018c628d14d45617605040ec1de4b
TheStachelfisch <TheStachelfisch@users.noreply.github.com>
2021-07-30 11:45:57 +0200

    Code commit

---
+++ LinkFixerBot/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+namespace LinkFixerBot
+{
+    class Program
+    {
+        public readonly string GatewayToken = File.ReadAllText("token.ignore");
+        private DiscordSocketClient _client;
+
+        static void Main(string[] args) => new Program().StartAsync().GetAwaiter().GetResult();
+
+        public async Task StartAsync()
+        {
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Info,
+                ConnectionTimeout = 30000,
+                HandlerTimeout = 5000,
+                DefaultRetryMode = RetryMode.RetryRatelimit,
+                UseSystemClock = true
+            });
+
+            _client.Log += message =>
+            {
+                Console.WriteLine(message.ToString());
+                return Task.CompletedTask;
+            };
+            
+            await _client.LoginAsync(TokenType.Bot, GatewayToken);
+            await _client.StartAsync();
+
+            Task ReadyEvent()
+            {
+                _client.Ready -= ReadyEvent;
+                
+                SetupAsync().GetAwaiter().GetResult();
+
+                return Task.CompletedTask;
+            }
+
+            _client.Ready += ReadyEvent;
+            
+            await Task.Delay(-1);
+        }
+
+        private async Task SetupAsync()
+        {
+            _client.MessageReceived += FixMessageHandler;
+        }
+
+        private async Task FixMessageHandler(SocketMessage message)
+        {
+            var brokenLink = message.Embeds.FirstOrDefault(x => x.Type == EmbedType.Video)?.Url;
+            if (brokenLink == null)
+                return;
+            
+            
+            if (brokenLink.Contains("https://media.discordapp.net/attachments/") || brokenLink.Contains("http://media.discordapp.net/attachments/") && !message.Author.IsBot)
+                await ((IUserMessage) message).ReplyAsync(brokenLink.ReplaceFirst("media", "cdn").ReplaceFirst(".net", ".com"), allowedMentions: AllowedMentions.None);
+        }
+    }
+}
---
+++ LinkFixerBot/StringExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LinkFixerBot
+{
+    public static class StringExtensionMethods
+    {
+        public static string ReplaceFirst(this string text, string search, string replace)
+        {
+            int pos = text.IndexOf(search, StringComparison.Ordinal);
+            if (pos < 0)
+            {
+                return text;
+            }
+            return text.Substring(0, pos) + replace + text.Substring(pos + search.Length);
+        }
+    }
+}
